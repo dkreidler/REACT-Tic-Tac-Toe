52a0e4ab20564e934cd32d129b94bd06f71c6ecb
dkreidler <dkreidler@gmail.com>
2018-05-02 21:39:39 -0400

    Tic-tac-toe to github

---
+++ src/components/board.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Square} from './square.js';
+
+export class Board extends React.Component {
+    renderSquare(i) {
+      return (
+        <Square
+          value={this.props.squares[i]} 
+          onClick={() => this.props.onClick(i)}
+        />
+      );  
+    }
+  
+    render() {
+      return (
+        <div>
+          <div className="board-row">
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            {this.renderSquare(3)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+            {this.renderSquare(6)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+            {this.renderSquare(9)}
+          </div>
+        </div>
+      );
+    }
+}
